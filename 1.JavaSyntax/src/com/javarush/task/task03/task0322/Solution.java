c4538c6d823b495f5e80310cb45867cdea4204ec
ntertysh <natalya.tertishnaya@gmail.com>
2017-06-01 22:32:10 -0700

    Solution - task 322

--- 1.JavaSyntax/src/com/javarush/task/task03/task0322/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0322/Solution.java
@@ -10,5 +10,14 @@ import java.io.*;
 public class Solution {
     public static void main(String[] args) throws Exception {
         //напишите тут ваш код
+        InputStream inputStream = System.in;
+        Reader inputStreamReader = new InputStreamReader(inputStream);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+        String name1 = bufferedReader.readLine();
+        String name2 = bufferedReader.readLine();
+        String name3 = bufferedReader.readLine();
+
+        System.out.print(name1 + " + " + name2 + " + " + name3 + " = Чистая любовь, да-да!");
     }
 }
