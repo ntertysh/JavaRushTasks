237350a4a226f6906390f7b88ecf0aed0b0b2eb7
ntertysh <natalya.tertishnaya@gmail.com>
2017-06-11 13:47:12 -0700

    Solution - 416

--- 1.JavaSyntax/src/com/javarush/task/task04/task0414/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0414/Solution.java
@@ -9,5 +9,20 @@ import java.io.*;
 public class Solution {
     public static void main(String[] args) throws Exception {
         //напишите тут ваш код
+        InputStream inputStream = System.in;
+        Reader inputStreamReader = new InputStreamReader(inputStream);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+        String a = bufferedReader.readLine();
+        int y = Integer.parseInt(a);
+
+        if (y%400 != 0 && y%100 == 0 || y%4 != 0) {
+            int x = 365;
+            System.out.println("количество дней в году: " + x);
+        }
+        else {
+            int x = 366;
+            System.out.println("количество дней в году: " + x);
+        }
+        }
     }
-}
