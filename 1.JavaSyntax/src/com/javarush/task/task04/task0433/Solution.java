ccaad83b6ecdd16c97770e8d11e4f169af458866
ntertysh <natalya.tertishnaya@gmail.com>
2017-06-22 21:51:40 -0700

    Solution - 433

---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0433/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task04.task0433;
+
+
+/* 
+Гадание на долларовый счет
+*/
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+        int j = 0;
+
+        while (j<10) {
+            int i=0;
+            while (i<10) {
+                System.out.print("S");
+                i++;
+            }
+            System.out.println("");
+            j++;
+        }
+    }
+}
