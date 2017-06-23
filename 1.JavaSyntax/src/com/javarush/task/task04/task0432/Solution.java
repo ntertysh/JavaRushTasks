01f09f7bc93a95c1203094907fed8737d7cbe559
ntertysh <natalya.tertishnaya@gmail.com>
2017-06-22 18:28:54 -0700

    Solution - 432

---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0432/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task04.task0432;
+
+
+
+/* 
+Хорошего много не бывает
+*/
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+        InputStream inputStream = System.in;
+        Reader inputStreamReader = new InputStreamReader(inputStream);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+        String a = bufferedReader.readLine();
+        String b = bufferedReader.readLine();
+        int x = Integer.parseInt(b);
+
+        int i = 0;
+        while (i<x)
+        {
+            System.out.println(a);
+            i++;
+        }
+    }
+}
