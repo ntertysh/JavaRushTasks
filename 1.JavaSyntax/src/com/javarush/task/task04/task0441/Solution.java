f835747bafc6168b06355a84e4cadbbe2497bdef
ntertysh <natalya.tertishnaya@gmail.com>
2017-06-29 16:38:42 -0700

    Solution - 441

--- 1.JavaSyntax/src/com/javarush/task/task04/task0441/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0441/Solution.java
@@ -20,16 +20,27 @@ public class Solution {
         int b = Integer.parseInt(y);
         int c = Integer.parseInt(z);
 
-        if (a>=b && a>=c && b>=c) {
+        if (a >= b && a >= c && b >= c) {
             System.out.println(b);
         }
-        else if (b>=a && b>=c && a>=c) {
-            System.out.println(a);
-        }
-        else if (c>=a && c>=b && a>=b) {
+        else if (a >= b && a >= c && c >= b) {
             System.out.println(c);
         }
-
-
+        else if (b >= a && b >= c && a >= c) {
+            System.out.println(a);
         }
+        else if (b >= a && b >= c && c >= a) {
+            System.out.println(c);
+        }
+        else if (c >= a && c >= b && a >= b) {
+            System.out.println(a);
+        }
+        else if (c >= a && c >= b && b >= a) {
+            System.out.println(b);
+        }
+        else if (a==b && a==c) {
+            System.out.println(a);
+        }
+
     }
+}
