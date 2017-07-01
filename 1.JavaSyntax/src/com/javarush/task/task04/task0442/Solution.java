c1af64e315460d3d8b110cd69b32b77e78eb4b24
ntertysh <natalya.tertishnaya@gmail.com>
2017-07-01 14:14:46 -0700

    Solution - 442

--- 1.JavaSyntax/src/com/javarush/task/task04/task0442/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0442/Solution.java
@@ -10,5 +10,23 @@ import java.io.*;
 public class Solution {
     public static void main(String[] args) throws Exception {
         //напишите тут ваш код
+        InputStream inputStream = System.in;
+        Reader inputStreamReader = new InputStreamReader(inputStream);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+        String x = bufferedReader.readLine();
+        String y = bufferedReader.readLine();
+        int a = Integer.parseInt(x);
+        int b = Integer.parseInt(y);
+        int c = a+b;
+
+        while (a!=-1 || b!=-1) {
+            if (a == -1 || b == -1) {
+                System.out.println(c);
+                break;
+            }
+            System.out.println();
+        }
+
     }
 }
