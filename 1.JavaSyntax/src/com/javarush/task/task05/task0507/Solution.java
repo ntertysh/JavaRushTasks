281d83ab9a5704eae4af79da1f80f0f63852f217
ntertysh <natalya.tertishnaya@gmail.com>
2017-07-02 18:43:52 -0700

    Solution - 508

--- 1.JavaSyntax/src/com/javarush/task/task05/task0507/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0507/Solution.java
@@ -4,9 +4,32 @@ package com.javarush.task.task05.task0507;
 Среднее арифметическое
 */
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
 public class Solution {
     public static void main(String[] args) throws Exception {
         //напишите тут ваш код
+
+        InputStream inputStream = System.in;
+        Reader inputStreamReader = new InputStreamReader(inputStream);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+        int count = 0;
+        int sum = 0;
+
+        while (true) {
+            String a = bufferedReader.readLine();
+            int shit = Integer.parseInt(a);
+            if (shit != -1) {
+                count = count + 1;
+                sum = sum + shit;
+            } else break;
+        }
+            System.out.println((float)sum / count);
+
+        }
     }
-}
 
